a890d224e0960b3bbf8938e81c1d1fb12b63e4b9
Burhonov <38163059+Burhonov@users.noreply.github.com>
2020-12-11 06:54:36 +0500

    feat: basic covid api methods and state util

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import State from 'utils/State';
 import './styles/main.scss';
 import { CountryList, InfoTable, WorldMap } from './components';
 
+State.init();
 const rootNode = document.getElementById('root');
 
 rootNode.appendChild(InfoTable());
---
+++ src/utils/constants.js
@@ -0,0 +1,20 @@
+export const PERIODS = {
+  TOTAL: 'Total',
+  NEW: 'New',
+};
+
+export const UNITS = {
+  ABSOLUTE: 'Absolute',
+  RELATIVE: 'Relative',
+};
+
+export const REGIONS = {
+  GLOBAL: 'Global',
+  COUNTRIES: 'Countries',
+};
+
+export const STATUSES = {
+  CONFIRMED: 'Confirmed',
+  DEATHS: 'Deaths',
+  RECOVERED: 'Recovered',
+};
