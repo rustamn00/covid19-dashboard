132e844e503e72fb25baf4da6ada98cee945caf5
Burhonov <38163059+Burhonov@users.noreply.github.com>
2020-12-23 19:35:18 +0500

    fix: add jest plugin to eslint and use esm modules

--- src/components/InfoTable/index.test.js
+++ src/components/InfoTable/index.test.js
@@ -1,26 +1,21 @@
-const obj = require('./index')
+import obj from './index';
 
 test('InfoTable content should be defined', () => {
-    
-    expect(obj.getDataObj()).toBeDefined();
-})
+  expect(obj.getDataObj()).toBeDefined();
+});
 
 test('InfoTable content should be div', () => {
-    
-    expect(Object.entries(obj.getDataObj)).not.toEqual(0);
-})
+  expect(Object.entries(obj.getDataObj)).not.toEqual(0);
+});
 
 test('Data about Covid should be defined', () => {
-    
-    expect(obj.getRealData()).toBeDefined();
-})
+  expect(obj.getRealData()).toBeDefined();
+});
 
 test('Data about Covid should be an object', () => {
-
-    expect(obj.getRealData()).toBeInstanceOf(Object);
-})
+  expect(obj.getRealData()).toBeInstanceOf(Object);
+});
 
 test('Data about Covid should exist', () => {
-    
-    expect(obj.getRealData().length).toBe(1);
-})
+  expect(obj.getRealData().length).toBe(1);
+});
